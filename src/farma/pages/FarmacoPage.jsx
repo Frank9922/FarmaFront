834e6d3dd9502069eea3ccad5e799a61d5631b2c
EliasP05 <eliasalberto0505@gmail.com>
2024-09-26 12:46:21 -0300

    .,

--- src/farma/pages/FarmacoPage.jsx
+++ src/farma/pages/FarmacoPage.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { FarmaLayout } from '../layout/FarmaLayout'
-import { useGetFarmacoQuery } from '../../store/apis/checkApi'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { FarmaLayout } from "../layout/FarmaLayout";
+import { useGetFarmacoQuery } from "../../store/apis/checkApi";
 
 const FarmacoPage = () => {
-    const {FarmaName}=useParams()
-    console.log(FarmaName)
-    const {isFetching, data} =useGetFarmacoQuery(FarmaName)
-    
-    return (
+  const { FarmaName } = useParams();
+  console.log(FarmaName);
+  const { isFetching, data } = useGetFarmacoQuery(FarmaName);
+
+  return (
     <FarmaLayout>
       <div>
-      
-      <h1>{FarmaName}</h1>
-      {
-         isFetching ? <div className="spinner"></div> : JSON.stringify(data)
-      }
-    </div>
+        <h1>{FarmaName}</h1>
+        {isFetching ? (
+          <div className="spinner"></div>
+        ) : (
+          <div>
+            {data && data.farmacos ? (
+              <table>
+                <thead>
+                  <tr>
+                    <th>Nombre</th>
+                    <th>Dosis</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.farmaco.map((farmaco) => (
+                    <tr key={farmaco.id}>
+                      <td>{farmaco.name}</td>
+                      <td>{farmaco.accion_terapeutica}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p>No hay datos disponibles</p>
+            )}
+          </div>
+        )}
+      </div>
     </FarmaLayout>
-    
-  )
-}
+  );
+};
 
-export default FarmacoPage
+export default FarmacoPage;
