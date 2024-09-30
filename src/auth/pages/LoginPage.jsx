8522ecfdb2eca3aa8baa2ed3e71157ad5983c2d5
EliasP05 <eliasalberto0505@gmail.com>
2024-09-30 08:19:49 -0300

    login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,50 +1,56 @@
-import { Link, useNavigate } from "react-router-dom"
-import { AuthLayout } from "../../farma/layout/AuthLayout"
-import { motion } from "framer-motion"
-import { useDispatch, useSelector } from "react-redux"
-import { useState } from "react"
-import { useForm } from "../hooks/useForm"
-import { startLoginWithEmalAndPassword } from "../../store/slices/auth/thunks"
-import { estados } from "../../store/slices/auth/estados"
+import { Link, useNavigate } from "react-router-dom";
+import { AuthLayout } from "../../farma/layout/AuthLayout";
+import { motion } from "framer-motion";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { useForm } from "../hooks/useForm";
+import { startLoginWithEmalAndPassword } from "../../store/slices/auth/thunks";
+import { estados } from "../../store/slices/auth/estados";
 
 const initialForm = {
-  email: '',
-  password: ''
-}
+  email: "",
+  password: "",
+};
 
 const formValidation = {
-  email: [(value) => value.includes('@'), 'El correo debe tener una @'],
-  password: [(value) => value.length > 3, 'La contraseña debe tener al menos 6 caracteres']
-}
+  email: [(value) => value.includes("@"), "El correo debe tener una @"],
+  password: [
+    (value) => value.length > 3,
+    "La contraseña debe tener al menos 6 caracteres",
+  ],
+};
 
 export const LoginPage = () => {
-
   const navigate = useNavigate();
 
-  const auth = useSelector(state => state.auth);
-  
+  const auth = useSelector((state) => state.auth);
+
   const dispatch = useDispatch();
 
   const [formSubmitted, setFormSubmitted] = useState(false);
 
-  const { email, emailValid, password, passwordValid, formState, isFormValid, onInputChange} = useForm(initialForm, formValidation);
+  const {
+    email,
+    emailValid,
+    password,
+    passwordValid,
+    formState,
+    isFormValid,
+    onInputChange,
+  } = useForm(initialForm, formValidation);
 
+  const onSumbit = async (event) => {
+    event.preventDefault();
 
+    setFormSubmitted(true);
 
-  const onSumbit = async(event) => {
-      event.preventDefault();
+    if (!isFormValid) return;
 
-      setFormSubmitted(true);
+    await dispatch(startLoginWithEmalAndPassword(formState));
 
-      if(!isFormValid) return 
+    if (auth.estado === estados.autenticado) navigate("/compatibilidad");
+  };
 
-      await dispatch(startLoginWithEmalAndPassword(formState));
-
-      if(auth.estado === estados.autenticado) navigate('/compatibilidad')
-
-
-  }
- 
   return (
     <AuthLayout>
       <motion.div
@@ -52,66 +58,68 @@ export const LoginPage = () => {
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: "-20%", opacity: 0, transition: { duration: 0.2 } }}
         transition={{ delay: 0, duration: 0.2 }}
-      >      <form onSubmit={onSumbit}>
+      >
+        {" "}
+        <form onSubmit={onSumbit}>
+          <div className="head-form">
+            <h1>Inicio de Sesion</h1>
+            <div className="line"></div>
+          </div>
 
-                <div className="head-form">
-                    <h1>Inicio de Sesion</h1>
-                    <div className="line"></div>
-                </div>
+          <div className="body-form">
+            <input
+              type="email"
+              name="email"
+              autoComplete="email"
+              value={email}
+              disabled={auth.estado === "checking"}
+              onChange={onInputChange}
+              className={` ${!!emailValid && formSubmitted ? "error" : ""}`}
+              placeholder="Email"
+            />
 
+            <input
+              type="password"
+              name="password"
+              autoComplete="password"
+              disabled={auth.estado === "checking"}
+              value={password}
+              onChange={onInputChange}
+              className={` ${!!passwordValid && formSubmitted ? "error" : ""}`}
+              placeholder="Contraseña"
+            />
 
-                <div className="body-form">
-
-                    <input 
-                    type="email"
-                    name="email"
-                    autoComplete="email"
-                    value={email}
-                    disabled={auth.estado === 'checking'}
-                    onChange={onInputChange}
-                    className={` ${!!emailValid && formSubmitted ? 'error' : ''}`}
-                    placeholder="Email" 
-                    />
-
-                    <input 
-                    type="password" 
-                    name="password"
-                    autoComplete="password"
-                    disabled={auth.estado === 'checking'}
-                    value={password}
-                    onChange={onInputChange}
-                    className={` ${!!passwordValid && formSubmitted ? 'error' : ''}`}
-                    placeholder="Contraseña" 
-                    />
-
-                            <div className={`error-form-alert ${!isFormValid && formSubmitted ? 'show' : ''}`}>
-                              Por favor complete los campos obligatorios
-                            </div>
-
-                </div>
-
-
-                <div className="footer-form">
-                <button disabled={ auth.estado === 'checking'} >
-                        { auth.estado === 'checking' ? (<div className="spinner"></div>) : 'Iniciar Sesion' }
-                    </button>
-                </div>
-
-
-                <div className="body-form">
-                  <Link to='/register'>¿No tienes cuenta? Create una</Link>
-                </div>
-
-      </form>
-
-
-
-            <div className="alerta">
-                <div className="message">
-                    <h5>Usuario o contraseña icorrectos</h5>
-                </div>
+            <div
+              className={`error-form-alert ${
+                !isFormValid && formSubmitted ? "show" : ""
+              }`}
+            >
+              Por favor complete los campos obligatorios
             </div>
+          </div>
+
+          <div className="footer-form">
+            <button disabled={auth.estado === "checking"}>
+              {auth.estado === "checking" ? (
+                <div className="spinner"></div>
+              ) : (
+                "Iniciar Sesion"
+              )}
+            </button>
+          </div>
+
+          <div className="body-form">
+            <Link className="farmaLink" to="/register">
+              ¿No tienes cuenta? Create una
+            </Link>
+          </div>
+        </form>
+        <div className="alerta">
+          <div className="message">
+            <h5>Usuario o contraseña icorrectos</h5>
+          </div>
+        </div>
       </motion.div>
     </AuthLayout>
-  )
-}
+  );
+};
