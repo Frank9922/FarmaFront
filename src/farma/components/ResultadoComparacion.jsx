bd4f5b30839699aa8785648ce0905ac74e4987c9
EliasP05 <eliasalberto0505@gmail.com>
2024-09-30 11:07:59 -0300

    icono- IA

--- src/farma/components/ResultadoComparacion.jsx
+++ src/farma/components/ResultadoComparacion.jsx
@@ -1,95 +1,79 @@
-import PropTypes from 'prop-types';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { useGetInfoComparacionIaQuery } from '../../store/apis/checkApi';
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { useGetInfoComparacionIaQuery } from "../../store/apis/checkApi";
 
+export const ResultadoComparacion = ({ compa }) => {
+  const [skip, setSkip] = useState(true);
 
-export const ResultadoComparacion = ({compa}) => {
+  const farma1 = compa.first_farmaco.name;
+  const farma2 = compa.second_farmaco.name;
 
-    const [skip, setSkip] = useState(true)
+  const farmacos = [farma1, farma2];
 
-    const farma1 = compa.first_farmaco.name
-    const farma2 = compa.second_farmaco.name
+  const { isFetching, data } = useGetInfoComparacionIaQuery(farmacos, {
+    skip: skip,
+  });
 
-    const farmacos = [farma1, farma2]
+  const color = (nombre) => {
+    const string = nombre.toLowerCase();
 
-    const { isFetching, data } = useGetInfoComparacionIaQuery(farmacos, {skip : skip});
+    return string.replace(/\s/g, "");
+  };
 
-    const color = (nombre) => {
-
-        const string = nombre.toLowerCase();
-        
-        return string.replace(/\s/g, '')
-
-    }
-
-    const onSumbit = async() => {
-        
-        setSkip(false);
-        
-    }
+  const onSumbit = async () => {
+    setSkip(false);
+  };
 
   return (
+    <motion.div
+      className={`recent-orders ${color(compa.compatibilidad.name)}`}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      <div className="head-result">
+        <span className="material-symbols-outlined">monitoring</span>
+        <h2>Resultado</h2>
+      </div>
 
-        <motion.div
-        className={`recent-orders ${color(compa.compatibilidad.name)}`}
-        initial={{opacity: 0}}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
-        >
-    
-            <div className="head-result">
-                <span className="material-symbols-outlined">monitoring</span>
-                <h2>Resultado</h2>
+      <div className="middle">
+        <div className="left">
+          <h1>{compa.compatibilidad.name}</h1>
+        </div>
+
+        <div className="progreso">
+          {skip ? (
+            <div className="ia">
+              <button className="btn" onClick={onSumbit}>
+                <span class="material-symbols-outlined">automation</span>
+                <div>Consultar con IA</div>
+              </button>
             </div>
-
-            <div className="middle">
-
-                <div className="left">
-                    <h1>{compa.compatibilidad.name}</h1>
-                </div>
-
-                <div className="progreso">
-                    {
-                        skip ? 
-                        <div className="ia">
-                            <button className='btn' onClick={onSumbit}>
-                            Consultar con IA
-                            </button>
-                        </div>
-                         : null
-                    }
-                </div>
-                {
-                    !skip ? 
-                    <div className='ia-contenedor'>
-                        
-                        {
-                            isFetching ? <div className='spinner'></div> : <p>{data.text}</p>
-                        }
-
-                    </div>
-                    : null
-                }
-            </div>
-        </motion.div>
-    );
+          ) : null}
+        </div>
+        {!skip ? (
+          <div className="ia-contenedor">
+            {isFetching ? <div className="spinner"></div> : <p>{data.text}</p>}
+          </div>
+        ) : null}
+      </div>
+    </motion.div>
+  );
 };
 
 ResultadoComparacion.propTypes = {
-    compa: PropTypes.shape({
-        first_farmaco: PropTypes.shape({
-            name: PropTypes.string,
-        }),
-        second_farmaco: PropTypes.shape({
-            name: PropTypes.string,
-        }),
-        compatibilidad: PropTypes.shape({
-            name: PropTypes.string,
-            color: PropTypes.string
-        })
+  compa: PropTypes.shape({
+    first_farmaco: PropTypes.shape({
+      name: PropTypes.string,
     }),
-  };
-  
-
+    second_farmaco: PropTypes.shape({
+      name: PropTypes.string,
+    }),
+    compatibilidad: PropTypes.shape({
+      name: PropTypes.string,
+      color: PropTypes.string,
+    }),
+  }),
+};
