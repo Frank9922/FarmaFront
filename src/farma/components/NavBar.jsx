06eb7b42d5ba94f02203a8b988ede56096b1b6f4
EliasP05 <eliasalberto0505@gmail.com>
2024-09-30 08:46:05 -0300

    titulo

--- src/farma/components/NavBar.jsx
+++ src/farma/components/NavBar.jsx
@@ -1,53 +1,60 @@
-import { useDispatch, useSelector } from "react-redux"
-import { startLogout } from "../../store/slices/auth/thunks"
-import { NavLink } from "react-router-dom"
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from "react-redux";
+import { startLogout } from "../../store/slices/auth/thunks";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
 import { closeNavbar } from "../../store/slices/ui/uiSlice";
 import { useEffect } from "react";
 
-
 export const NavBar = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const { menuOpen } = useSelector((state) => state.ui);
 
-    const { menuOpen } = useSelector(state => state.ui)
+  const onLogout = () => {
+    dispatch(startLogout());
+  };
 
-    const onLogout = () => {
-        dispatch(startLogout());
-    }
+  const closeMenu = () => {
+    dispatch(closeNavbar());
+  };
 
-    const closeMenu = () => {
-      dispatch(closeNavbar());
-    }
-
-    useEffect(() => {
-
-      dispatch(closeNavbar())
-
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(closeNavbar());
+  }, [dispatch]);
 
   return (
-<aside style={{ display: menuOpen === 'true' ? 'block' : '' }}>
-    <div className="top">
-      <div className="logo">
-        {/* <img src="" alt="" /> */}
-        <h2 className="" ><span className="danger">FarmaCheck</span></h2>
+    <aside style={{ display: menuOpen === "true" ? "block" : "" }}>
+      <div className="top">
+        <div className="logo">
+          {/* <img src="" alt="" /> */}
+          <h2 className="">
+            <span className="danger">Enfar-MedGuide</span>
+          </h2>
+        </div>
+        <div className="close" id="close-btn" onClick={closeMenu}>
+          <span className="material-symbols-outlined">close</span>
+        </div>
       </div>
-      <div className="close" id="close-btn" onClick={closeMenu}>
-        <span className="material-symbols-outlined">close</span>
-      </div>
-    </div>
       <div className="sidebar">
-        <NavLink to='/compatibilidad' className={({ isActive }) => (isActive ? 'active' : '')}>
+        <NavLink
+          to="/compatibilidad"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
           <span className="material-symbols-outlined">compare</span>
           <h3>Compatibilidad</h3>
         </NavLink>
-        <NavLink to='/medicamentos' className={({ isActive }) => (isActive ? 'active' : '')}>
+        <NavLink
+          to="/medicamentos"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
           <span className="material-symbols-outlined">syringe</span>
-           {/* vaccines  */}
+          {/* vaccines  */}
           <h3>Farmacos</h3>
         </NavLink>
-        <NavLink to='/about' className={({ isActive }) => (isActive ? 'active' : '')}>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
           <span className="material-symbols-outlined">info</span>
           <h3>Acerca de</h3>
         </NavLink>
@@ -56,10 +63,9 @@ export const NavBar = () => {
           <h3>Salir</h3>
         </a>
       </div>
-  </aside>
-
-)
-}
+    </aside>
+  );
+};
 NavBar.propTypes = {
-  isActiveMenu: PropTypes.bool
-}
+  isActiveMenu: PropTypes.bool,
+};
--- src/farma/pages/AcercadePage.jsx
+++ src/farma/pages/AcercadePage.jsx
@@ -7,21 +7,21 @@ export const AcercadePage = () => {
         <h1 className="acercadeTitulo">Acerca de</h1>
         <div className="acercadeText">
           <p>
-            <b>FarmaCheck</b> tiene como objetivo proporcionar información
+            <b>Enfar-MedGuide</b> tiene como objetivo proporcionar información
             confiable y precisa sobre la compatibilidad entre diferentes
-            medicamentos. Esta herramienta está diseñada tanto para profesionales
-            de la salud como para usuarios en general, ayudando a tomar decisiones
-            informadas acerca del uso de fármacos. La información que ofrecemos
-            está basada en estudios clínicos revisados y extraída de fuentes
-            científicas de confianza, incluyendo bases de datos oficiales de
-            interacciones farmacológicas como :
+            medicamentos. Esta herramienta está diseñada tanto para
+            profesionales de la salud como para usuarios en general, ayudando a
+            tomar decisiones informadas acerca del uso de fármacos. La
+            información que ofrecemos está basada en estudios clínicos revisados
+            y extraída de fuentes científicas de confianza, incluyendo bases de
+            datos oficiales de interacciones farmacológicas como :
             <ul>
               <li>
                 <a
                   href="http://revistapediatria.com.ar/wp-content/uploads/2021/04/GuiaAdministracionMedicamentos_3ED.-1.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
-className="farmaLink"
+                  className="farmaLink"
                 >
                   Guia para la Administacion parental de medicamentos.
                 </a>
@@ -31,7 +31,7 @@ className="farmaLink"
                   href=" https://www.garrahan.gov.ar/PDFS/medicamentos/tablas/Compatibilidad_en_Y.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
-className="farmaLink"
+                  className="farmaLink"
                 >
                   Hospital Garrahan - Compatibilidad de medicamentos
                   administrados. en Y.
@@ -55,19 +55,24 @@ className="farmaLink"
             investigaciones. Es importante destacar que el contenido de este
             sistema es de carácter orientativo y no sustituye el consejo médico
             profesional. Recomendamos que cualquier decisión relacionada con
-            tratamientos o uso de fármacos se tome siempre bajo la supervisión de
-            un especialista.
+            tratamientos o uso de fármacos se tome siempre bajo la supervisión
+            de un especialista.
           </p>
           <p>
             Este proyecto fue originalmente concebido por{" "}
             <span>Lic. Saavedra</span>, y ha sido desarrollado por{" "}
-            <span> <a href="" className="farmaLink">AwareWeb</a></span>, quienes han trabajado
-            en conjunto para llevar esta idea a la realidad. Si tienes alguna
-            pregunta, sugerencia o deseas más información, puedes contactarnos a
-            través de nuestro correo electrónico: info@farmaCheck.com. Nos
-            esforzamos por ofrecer un servicio de calidad que facilite el acceso a
-            información valiosa para mejorar la seguridad y efectividad en el uso
-            de medicamentos.
+            <span>
+              {" "}
+              <a href="" className="farmaLink">
+                AwareWeb
+              </a>
+            </span>
+            , quienes han trabajado en conjunto para llevar esta idea a la
+            realidad. Si tienes alguna pregunta, sugerencia o deseas más
+            información, puedes contactarnos a través de nuestro correo
+            electrónico: info@Enfar-MedGuide.com. Nos esforzamos por ofrecer un
+            servicio de calidad que facilite el acceso a información valiosa
+            para mejorar la seguridad y efectividad en el uso de medicamentos.
           </p>
         </div>
       </>
